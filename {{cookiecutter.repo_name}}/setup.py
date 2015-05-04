e90e1533d33e27221320308ee81e7456928c9350
Michael Messmore <michael.messmore@fedex.com>
2015-05-04 14:32:02 -0500

    Tweaks

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
 
-import os
-import sys
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit()
 
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
